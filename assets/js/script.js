7c596aa6a070d607ab58ae6ec653b7b8b6c5c2cb
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-06-17 08:18:09 +0000

    Add characters quiz section with image

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,8 @@ document.getElementById("back-button").addEventListener("click", function() {
     document.getElementById("quiz-selection").classList.add("hidden");
     document.querySelector(".main-area").style.display = "flex";
 }) ;
+
+document.getElementById("characters-quiz").addEventListener("click", function() {
+    document.getElementById("quiz-selection").classList.add("hidden");
+    document.getElementById("characters-section").classList.remove("hidden");
+}) ;
